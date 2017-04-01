108f45d0202c3f936a33a2ff0ddafc1f34a4c371
Thomas <tcserven@uoguelph.ca>
2017-04-01 19:25:39 -0400

    Add socket.io initial setup

--- server/server.js
+++ server/server.js
@@ -3,20 +3,31 @@
 
 const path = require('path');
 const express = require('express');
-var app = express();
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 // serve up the public folder
 app.use(express.static(publicPath));
 
+// registers for an event listener, in this case its connection. when a new user connects to the server. 
+io.on('connection', function(socket) {
+	console.log('New user connected');
+
+	socket.on('disconnect', function() {
+		console.log('User was disconnected from server');
+	});
+});
 
 
 
 
-
-
-app.listen(port, function() {
+server.listen(port, function() {
     console.log("server has started on " + port);
 });
