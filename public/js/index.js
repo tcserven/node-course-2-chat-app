fc72dee8082d089dbc9c56f6a75d45b3ba65e36a
Thomas <tcserven@uoguelph.ca>
2017-04-08 18:48:14 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 var socket = io();
 
+function scrollToBottom() {
+	// selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+
+	// heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	// prev goes to the previous child, so its 2nd last child of newMessage
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		// jquery messages
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function() {
 	console.log('Connected to server');
 
@@ -41,6 +61,7 @@ socket.on('newMessage', function(message) {
 	// li.text(message.from + " " + formattedTime + ": " + message.text);
 
 	// jQuery('#messages').append(li);
+	scrollToBottom();
 });
 
 // socket.emit('createMessage', { 
@@ -73,9 +94,11 @@ socket.on('newLocationMessage', function(message) {
 	// a.attr('href', message.url);
 	// li.append(a);
 	// jQuery('#messages').append(li);
+	scrollToBottom();
 });
 
 // preventing page reload
+// setting up for when a user submits a message 
 jQuery('#message-form').on('submit', function(event) {
 	event.preventDefault();
 
